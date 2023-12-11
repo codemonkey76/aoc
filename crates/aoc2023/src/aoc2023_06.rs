267df801e65d1a36f4c6d7e9cb06a5408a12c6aa
Shane Poppleton <shane@bjja.com.au>
2023-12-11 21:42:41 +1000

    Finished Day 6

--- crates/aoc2023/src/aoc2023_06.rs
+++ crates/aoc2023/src/aoc2023_06.rs
@@ -2,7 +2,7 @@ use aoclib::Runner;
 
 #[derive(Default)]
 pub struct Aoc2023_06 {
-    lines: Vec<String>
+    races: Vec<Race>
 }
 
 impl Aoc2023_06 {
@@ -18,14 +18,74 @@ impl Runner for Aoc2023_06 {
 
     fn parse(&mut self)
     {
-        self.lines = aoclib::read_lines(aoclib::get_test_path(self.name()));
+        let lines = aoclib::read_lines(aoclib::get_input_path(self.name()));
+        let (times, distances) = (get_numbers(&lines[0]), get_numbers(&lines[1]));
+        (0..times.len()).for_each(|i| {
+            self.races.push(Race {
+                time: times[i],
+                distance: distances[i]
+            })
+        });
     }
 
     fn part1(&mut self) -> Vec<String> {
-        aoclib::output("Unsolved")
+        let power = self.races.iter().fold(1, |acc, race| acc * race.max_winners());
+
+        aoclib::output(power)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("Unsolved")
+        let r = Race::combine(self.races.clone());
+
+        aoclib::output(r.max_winners())
+    }
+}
+
+fn get_numbers(str: &str) -> Vec<u64> {
+    str
+        .split_once(':')
+        .unwrap()
+        .1
+        .split_whitespace()
+        .map(|num| num.trim().parse::<u64>().unwrap())
+        .collect::<Vec<_>>()
+}
+
+#[derive(Debug, Default, Clone)]
+struct Race {
+    time: u64,
+    distance: u64
+}
+
+impl Race {
+    pub fn new(time: u64, distance: u64) -> Self {
+        Race { time, distance }
+    }
+
+    fn combine(races: Vec<Race>) -> Race {
+        races
+            .iter()
+            .fold(Race::default(), |acc, race| {
+                let time_digits = Race::get_digits(race.time);
+                let distance_digits = Race::get_digits(race.distance);
+
+                Race::new(
+                    acc.time * 10u64.pow(time_digits) + race.time,
+                    acc.distance * 10u64.pow(distance_digits) + race.distance)
+            })
+    }
+
+    fn get_digits(num: u64) -> u32 {
+        (num as f64).log10() as u32 + 1
+    }
+
+    fn max_winners(&self) -> u64 {
+        let mut winners = 0;
+        (1..self.time).for_each(|i| {
+            if i*(self.time-i)>self.distance {
+                winners += 1;
+            }
+        });
+        winners
     }
 }
