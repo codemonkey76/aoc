307b89d73bde43864952f86c8b52e4c9afd386f7
Shane Poppleton <shane@bjja.com.au>
2024-01-04 08:21:00 +1000

    Completed Day 13 - Part 2

--- crates/aoc2023/src/aoc2023_13.rs
+++ crates/aoc2023/src/aoc2023_13.rs
@@ -37,15 +37,17 @@ impl Runner for Aoc2023_13 {
     }
 
     fn part2(&mut self) -> i64 {
-        0
+        self.patterns.iter().map(|pattern| pattern.get_score2()).sum::<u64>() as i64
     }
 }
 
+#[derive(Debug, PartialEq)]
 enum Axis {
     Horizontal,
     Vertical
 }
 
+#[derive(Debug)]
 struct Reflection {
     axis: Axis,
     position: usize
@@ -75,10 +77,41 @@ impl Pattern {
         }
     }
 
+    fn get_score2(&self) -> u64 {
+        if let Some(reflection) = self.get_reflection() {
+            let score = reflection.score();
+            let map = self.map.clone();
+
+            for i in 0..map.len() {
+                for j in 0..map[0].len() {
+                    let mut new_pattern = Pattern { map: map.clone() };
+                    new_pattern.map[i][j] = !map[i][j];
+
+                    if let Some(new_reflection) = new_pattern.get_new_reflection(&reflection) {
+                        let new_score = new_reflection.score();
+                        if  new_score != score {
+                            return new_score;
+                        }
+                    }
+                }
+            }
+            panic!("No new line of reflection found for pattern");
+        } else {
+            println!("Could not find reflection for pattern");
+            println!("{}", self);
+            0
+        }
+    }
+
     fn get_reflection_pos(map: &[Vec<bool>]) -> Option<usize> {
+        Pattern::get_reflection_pos_with_ignore(map, None)
+    }
+
+    fn get_reflection_pos_with_ignore(map: &[Vec<bool>], ignore: Option<usize>) -> Option<usize> {
         let mut result = None;
 
         for i in 1..map.len() {
+            if Some(i) == ignore { continue; }
             let comparison_window = i.min(map.len()-i);
 
             if map[i-comparison_window..i].iter().rev().cloned().collect::<Vec<Vec<bool>>>() == map[i..i+comparison_window].to_vec() {
@@ -88,6 +121,35 @@ impl Pattern {
         }
 
         result
+    }
+
+    fn get_new_reflection(&self, old_reflection: &Reflection) -> Option<Reflection> {
+        match old_reflection.axis {
+            Axis::Horizontal => {
+                if let Some(pos) = Self::get_reflection_pos_with_ignore(&self.map, Some(old_reflection.position)) {
+                    return Some(Reflection { axis: Axis::Horizontal, position: pos });
+                }
+
+                let map = transpose(&self.map);
+
+                if let Some(pos) = Self::get_reflection_pos(&map) {
+                    return Some(Reflection{ axis: Axis::Vertical, position: pos });
+                }
+            }
+            Axis::Vertical => {
+                if let Some(pos) = Self::get_reflection_pos(&self.map) {
+                    return Some(Reflection { axis: Axis::Horizontal, position: pos });
+                }
+
+                let map = transpose(&self.map);
+
+                if let Some(pos) = Self::get_reflection_pos_with_ignore(&map, Some(old_reflection.position)) {
+                    return Some(Reflection{ axis: Axis::Vertical, position: pos });
+                }
+            }
+        }
+
+        None
     }
 
     fn get_reflection(&self) -> Option<Reflection> {
@@ -103,6 +165,7 @@ impl Pattern {
 
         None
     }
+
 }
 
 impl Display for Pattern {
@@ -153,6 +216,6 @@ mod tests {
         day.parse();
         let result = day.part2();
 
-        assert_eq!(0, result);
+        assert_eq!(400, result);
     }
 }
