7184f3f4de97820fbb81eed5bd233ad3f770f82d
Shane Poppleton <shane@bjja.com.au>
2024-01-08 11:15:43 +1000

    Completed Day 15 - Part 1

--- crates/aoc2023/src/aoc2023_15.rs
+++ crates/aoc2023/src/aoc2023_15.rs
@@ -4,6 +4,7 @@ use aoclib::{get_repo_root, Runner};
 #[derive(Default)]
 pub struct Aoc2023_15 {
     input: PathBuf,
+    steps: Vec<String>
 }
 
 impl Aoc2023_15 {
@@ -21,15 +22,29 @@ impl Runner for Aoc2023_15 {
         self.input = get_repo_root().join(input)
     }
 
-    fn parse(&mut self) {}
+    fn parse(&mut self) {
+        self.steps = aoclib::read_full(&self.input).split(',').map(|s| s.to_string()).collect();
+    }
 
     fn part1(&mut self) -> i64 {
-        0
+        self.steps.iter().fold(0, |acc, item| acc + get_hash(item))
     }
 
     fn part2(&mut self) -> i64 {
         0
     }
+}
+
+fn get_hash(item: &str) -> i64 {
+    // Determine the ASCII code for the current character of the string.
+    // Increase the current value by the ASCII code you just determined.
+    // Set the current value to itself multiplied by 17.
+    // Set the current value to the remainder of dividing itself by 256.
+    item.chars().fold(0, |acc, ch| {
+        let mut result = acc + (ch as u8) as i64;
+        result *= 17;
+        result % 256
+    })
 }
 
 #[cfg(test)]
